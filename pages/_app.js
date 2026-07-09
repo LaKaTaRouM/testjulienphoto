94af2fb7e24ebf7b892c1bdc7106a5b8906b594f
LaKaTaRouM <98712510+LaKaTaRouM@users.noreply.github.com>
2026-07-10 01:31:03 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    function blockImageContextMenu(e) {
+      if (e.target.tagName === 'IMG') e.preventDefault();
+    }
+    document.addEventListener('contextmenu', blockImageContextMenu);
+    return () => document.removeEventListener('contextmenu', blockImageContextMenu);
+  }, []);
+
   return <Component {...pageProps} />;
 }
